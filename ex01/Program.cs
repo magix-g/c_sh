1b4d42692553c9b3b38f6810bc69a70b05549831
me <a@a.com>
2022-09-20 17:57:22 +0300

    commit2

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, Worldzzzz!");
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+
+//Console.WriteLine("Введите ваше имя: ");
+//string username = Console.ReadLine();
+//Console.WriteLine("Привет, ");
+//Console.WriteLine(username);
+
+
+//Console.WriteLine("Hello, Worldzzzz!");
+
