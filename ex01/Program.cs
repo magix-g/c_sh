dbfadb5d2e3a76f24b23157f8c02e521cd432a0b
me <a@a.com>
2022-09-20 18:36:27 +0300

    random func

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,7 +1,17 @@
-﻿Console.Write("Введите ваше имя: ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
+
+//double numberA=11;
+//double numberB = 4;
+Console.Write(numberA);
+Console.Write(" / ");
+Console.WriteLine(numberB);
+Console.WriteLine(numberA/numberB);
+
+//Console.Write("Введите ваше имя: ");
+//string username = Console.ReadLine();
+//Console.Write("Привет, ");
+//Console.Write(username);
 
 //Console.WriteLine("Введите ваше имя: ");
 //string username = Console.ReadLine();
