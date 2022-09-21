b7ffa3cbb0d42d3e991ea555344db9d08cc6a77e
me <a@a.com>
2022-09-21 15:12:24 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+string n1, n2, n3;
+int max;
+
+Console.WriteLine("Введите три числа: ");
+Console.Write("Первое: ");
+n1 = Console.ReadLine();
+
+Console.Write("Второе: ");
+n2 = Console.ReadLine();
+
+Console.Write("Третье: ");
+n3 = Console.ReadLine();
+
+if(!int.TryParse(n1, out int result))
+ {Console.Write("Принимаются к сравнению только числа!");
+ return;}
+
+if(!int.TryParse(n2, out int result1))
+ {Console.Write("Принимаются к сравнению только числа!");
+ return;}
+
+if(!int.TryParse(n3, out int result2))
+ {Console.Write("Принимаются к сравнению только числа!");
+ return;}
+
+max=Convert.ToInt32(n1);  
+
+if(Convert.ToInt32(n2)>max)
+  max=Convert.ToInt32(n2); 
+ 
+if(Convert.ToInt32(n3)>max)
+  max=Convert.ToInt32(n3);  
+
+Console.Write("Максимальное из трех чисел: " + max);
