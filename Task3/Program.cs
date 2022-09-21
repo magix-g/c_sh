1797a1934cca8a8b23ed2602262ea85ecdaf80f9
me <a@a.com>
2022-09-21 16:30:01 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿string n1;
+double ost;
+
+Console.WriteLine("Введите число: ");
+n1 = Console.ReadLine();
+
+if(!double.TryParse(n1, out _))
+ {Console.Write("Принимается только число!");
+ return;}
+
+ost=Convert.ToDouble(n1); 
+
+if(ost%2>0)
+   Console.Write(n1 + " нечетное");
+else
+   Console.Write(n1 + " четное");
