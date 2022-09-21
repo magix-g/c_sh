0dc9ac8b5b3a3310b0dee53c97dee47b3b948264
me <a@a.com>
2022-09-21 16:42:13 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿string n1;
+int a;
+int s=0;
+Console.WriteLine("Введите число: ");
+n1 = Console.ReadLine();
+
+if(!double.TryParse(n1, out _))
+ {Console.Write("Принимается только число!");
+ return;}
+
+a=Convert.ToInt32(n1); 
+while(s<=a)
+{
+if(s%2==0)
+   Console.WriteLine(s + " - четное; ");
+s++;
+}
+   
