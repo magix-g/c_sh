3b08fe8d4a5b7a3118fbd91990663a56e3b595b1
me <a@a.com>
2022-09-29 17:19:32 +0300

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿string n1;
+
+Console.Write("Введите трехзначное число: ");
+n1=Console.ReadLine();
+
+if(!double.TryParse(n1, out _))
+ {Console.Write("Принимается только число!");
+ return;}
+
+if(n1.Length!=3)
+ {Console.Write("Принимается только трехзначное число!");
+ return;}
+
+Console.Write("Вторая цифра веденного числа: ");
+Console.Write(n1[1]);
