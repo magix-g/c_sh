a155ef39a72f402bfe4641a9acff3e8d39e3e30b
me <a@a.com>
2022-09-29 17:20:13 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿string x;
+Console.Write("Введите число: ");
+x = Console.ReadLine();
+
+if(!double.TryParse(x, out _))
+ {Console.Write("Принимается только число!");
+ return;}
+
+if (x.Length < 3)
+Console.Write("Третьего числа нет!");
+else
+Console.Write("Третья цифра заданного числа: " + x[2]); 
+
+
