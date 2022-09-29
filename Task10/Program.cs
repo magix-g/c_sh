fb95c119257762cb7c7446b5a71809a42f7d291d
me <a@a.com>
2022-09-29 17:21:38 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите целое число: ");
+string x = Console.ReadLine();
+
+if(!int.TryParse(x, out _))
+{
+      Console.Write("Принимается только целое число!");
+      return;
+}
+
+int a = Convert.ToInt32(x);
+int i = 1;
+
+while(i<=a)
+    {Console.WriteLine("Куб от " + i + " = " + i*i*i);
+    i++;}
